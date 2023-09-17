33e8df8f8e86e090826ca8e94221381a70962ece
Bob <rikhsiboev@gmail.com>
2023-09-16 22:14:41 -0600

    part - 3

---
+++ project-service/src/main/java/com/bob_r/impl/ProjectService.java
@@ -0,0 +1,28 @@
+package com.bob_r.impl;
+
+import com.bob_r.dto.ProjectDTO;
+import com.bob_r.entity.User;
+import com.bob_r.exception.*;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    ProjectDTO getByProjectCode(String code);
+
+    List<ProjectDTO> listAllProjects();
+
+    ProjectDTO save(ProjectDTO dto) throws ProjectServiceException;
+
+    ProjectDTO update(ProjectDTO dto) throws ProjectServiceException;
+
+    void delete(String code) throws ProjectServiceException;
+
+    ProjectDTO complete(String projectCode) throws ProjectServiceException;
+
+    List<ProjectDTO> listAllProjectDetails(String userName) throws ProjectServiceException;
+
+    List<ProjectDTO> readAllByAssignedManager(User user);
+
+    List<ProjectDTO> listAllNonCompletedProjects();
+}
---
+++ user-service/src/main/java/com/bob_r/service/RoleService.java
@@ -0,0 +1,12 @@
+package com.bob_r.service;
+
+import com.bob_r.dto.RoleDTO;
+import com.bob_r.exception.UserServiceException;
+
+import java.util.List;
+
+public interface RoleService {
+
+    List<RoleDTO> listAllRoles();
+    RoleDTO findById(Long id) throws UserServiceException;
+}
